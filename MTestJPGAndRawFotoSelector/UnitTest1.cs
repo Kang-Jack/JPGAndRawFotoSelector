5751f2c026151fd52a468ddf19f4a39edc400064
Lei Kang <laserk_klei@hotmail.com>
2022-10-20 16:51:36 +0800

    update to .net 4.8
    update to .net 4.8

--- MTestJPGAndRawFotoSelector/UnitTest1.cs
+++ MTestJPGAndRawFotoSelector/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace MTestJPGAndRawFotoSelector
         [TestMethod]
         public void canGetSettings()
         {
-            var file = @"G:\work\GitRepository\GitHub\JPGAndRawFotoSelector\JPGAndRawFotoSelector.xml";
+            var file = @".\JPGAndRawFotoSelector.xml";
             var settings = JPGRawFotoSelectorSettings.GetSettings(file);
             Assert.IsNotNull(settings);
             Assert.AreEqual(2,settings.Cameras.Length);
@@ -17,7 +17,7 @@ namespace MTestJPGAndRawFotoSelector
         [TestMethod]
         public void testEXIFManager()
         {
-            var exif = new ExifManager(@"F:\FotoOutput\_DSC4696.ARW");
+            var exif = new ExifManager(@".\DSC04550.JPG");
             Assert.IsNotNull(exif);
             exif.Dispose();
         }
